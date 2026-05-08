3a6c986b4d34d0a9341c85ce0e8486d2abb89e1c
Gaby Nieva <gaby.nieva@outlook.com>
2026-05-08 12:35:11 -0600

    Changing outputs

--- ProyectoHolaMundo.java
+++ ProyectoHolaMundo.java
@@ -1,5 +1,8 @@
 public class ProyectoHolaMundo{
     public static void main(String[] args){
         System.out.println("Hola mundo");
+        System.out.println("Bienvenidos a Java");
+        System.out.println("Y a Git/Github");
+
     }
 }
