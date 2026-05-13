4096f799852dd2659f7f898217dd9b42368f6d78
Gaby Nieva <gaby.nieva@outlook.com>
2026-05-13 13:09:27 -0600

    Sumando dos números

--- ProyectoHolaMundo.java
+++ ProyectoHolaMundo.java
@@ -1,8 +1,13 @@
 public class ProyectoHolaMundo{
+    
+
     public static void main(String[] args){
+        int a = 12, b = 14, c;
         System.out.println("Hola mundo");
         System.out.println("Bienvenidos a Java");
         System.out.println("Y a Git/Github");
+        c = a + b;
+        System.out.println("La suma de a + b es: " + c);
 
     }
 }
